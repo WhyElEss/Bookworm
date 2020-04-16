e04d2ff7db8047ec169c758fe1e10fb62a3b32be
Yuri Stanislavsky <stanislavskiy@gmail.com>
2020-04-16 14:02:33 +0300

    Initial Commit

---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Юрий Станиславский on 16.04.2020.
+//  Copyright © 2020 Yuri Stanislavsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
