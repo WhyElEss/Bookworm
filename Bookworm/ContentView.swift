60cfcc9d82d16ceb21369119eb8525ef2ac18fe7
Yuri Stanislavsky <stanislavskiy@gmail.com>
2020-04-17 15:41:55 +0300

    Project done

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -10,17 +10,26 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(entity: Book.entity(), sortDescriptors: [
+        NSSortDescriptor(keyPath: \Book.title, ascending: true),
+        NSSortDescriptor(keyPath: \Book.author, ascending: true),
+        NSSortDescriptor(keyPath: \Book.genre, ascending: true),
+    ]) var books: FetchedResults<Book>
 
     @State private var showingAddScreen = false
-    
+
     var body: some View {
         NavigationView {
             List {
                 ForEach(books, id: \.self) { book in
-                    NavigationLink(destination: Text(book.title ?? "Unknown Title")) {
-                        EmojiRatingView(rating: book.rating)
-                            .font(.largeTitle)
+                    NavigationLink(destination: DetailView(book: book)) {
+                        VStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            Text("Rating")
+                                .foregroundColor(.secondary)
+                                .font(.footnote)
+                        }
 
                         VStack(alignment: .leading) {
                             Text(book.title ?? "Unknown Title")
@@ -30,17 +39,31 @@ struct ContentView: View {
                         }
                     }
                 }
+            .onDelete(perform: deleteBooks)
             }
-                .navigationBarTitle("Bookworm")
-                .navigationBarItems(trailing: Button(action: {
-                    self.showingAddScreen.toggle()
-                }) {
-                    Image(systemName: "plus")
-                })
-                .sheet(isPresented: $showingAddScreen) {
-                    AddBookView().environment(\.managedObjectContext, self.moc)
-                }
+            .navigationBarTitle("Bookworm")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {
+                self.showingAddScreen.toggle()
+            }) {
+                Image(systemName: "plus")
+            })
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView().environment(\.managedObjectContext, self.moc)
+            }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our fetch request
+            let book = books[offset]
+
+            // delete it from the context
+            moc.delete(book)
+        }
+
+        // save the context
+        try? moc.save()
     }
 }
 
